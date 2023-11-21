a29a461b564f530c84b971b0212901c016c2f7ac
Praveen-Aththanayaka <aththanayakempa@gmail.com>
2023-11-21 19:52:28 +0530

    instanceOf Operator

--- src/main/java/inheritance/Zoo.java
+++ src/main/java/inheritance/Zoo.java
@@ -5,14 +5,25 @@ public class Zoo {
         Dog rocky = new Dog();
 //        rocky.fetch();
 //        rocky.makeSounds();
+//        feed(rocky);
 
         Animal sasha = new Dog();
+//        feed(sasha);
 //        sasha.makeSounds();
-        Dog sashaTheDog = (Dog) sasha;
-        sashaTheDog.fetch();
+//        Dog sashaTheDog = (Dog) sasha;
+//        sashaTheDog.fetch();
 
         sasha = new Cat();
-        sasha.makeSounds();
-        ((Cat)sasha).scratch();
+        feed(sasha);
+//        sasha.makeSounds();
+//        ((Cat)sasha).scratch();
+    }
+
+    public static void feed(Animal animal) {
+        if (animal instanceof Dog) {
+            System.out.println("Here's your dog food.");
+        } else if (animal instanceof Cat) {
+            System.out.println("Here's your cat food.");
+        }
     }
 }
